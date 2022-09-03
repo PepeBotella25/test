ab3cab40490a815631419b7943ebf08854f44572
Pelaez, Daniel <dpelaez@netsuite.com>
2022-09-03 11:20:19 -0300

    PDP first version

--- src/components/PDP/ItemDetails.tsx
+++ src/components/PDP/ItemDetails.tsx
@@ -1,11 +1,40 @@
 import React from "react";
+import "./ItemDetails.scss";
+import Row from "../ui/Row";
+import {getCurrencyFormatter} from "../../utils/Utils";
 
 interface Props {
-    item: Item;
+    item: ItemResponse["item"];
 }
 
 export default function ItemDetails(props: Props) {
     const { item } = props;
+    const { title, condition, picture, sold_quantity, price, description } = item;
+    const { currency, amount, decimals } = price;
 
-    return <div>{JSON.stringify(item)}</div>
+    const format = getCurrencyFormatter(currency);
+
+    return (
+        <div className={"itemDetails"}>
+            <Row>
+                <img src={picture} alt={"item image"} width={"680px"} height={"680px"}/>
+                <div className={"details"}>
+                    <div>
+                        <span>{condition}</span>
+                        <span>{" - "}</span>
+                        <span>{`${sold_quantity} vendidos`}</span>
+                    </div>
+                    <div className={"title"}>{title}</div>
+                    <div className={"price"}>{format(+[amount, decimals].join("."))}</div>
+                    <button>{"Comprar"}</button>
+                </div>
+            </Row>
+            <Row className={"itemDescription"}>
+                <div>
+                    <div className={"title"}>{"Descriptción del producto"}</div>
+                    <div>{description}</div>
+                </div>
+            </Row>
+        </div>
+    );
 }
